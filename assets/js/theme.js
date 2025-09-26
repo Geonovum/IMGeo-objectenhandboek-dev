968243bbd772ab59740c572e87df3e0064ade010
Arnoud de Boer <a.deboer@geonovum.nl>
2025-09-26 12:35:02 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,214 @@
-/* sphinx_rtd_theme version 0.4.1 | MIT license */
-/* Built 20180727 10:07 */
-require=function n(e,i,t){function o(s,a){if(!i[s]){if(!e[s]){var l="function"==typeof require&&require;if(!a&&l)return l(s,!0);if(r)return r(s,!0);var c=new Error("Cannot find module '"+s+"'");throw c.code="MODULE_NOT_FOUND",c}var u=i[s]={exports:{}};e[s][0].call(u.exports,function(n){var i=e[s][1][n];return o(i||n)},u,u.exports,n,e,i,t)}return i[s].exports}for(var r="function"==typeof require&&require,s=0;s<t.length;s++)o(t[s]);return o}({"sphinx-rtd-theme":[function(n,e,i){var jQuery="undefined"!=typeof window?window.jQuery:n("jquery");e.exports.ThemeNav={navBar:null,win:null,winScroll:!1,winResize:!1,linkScroll:!1,winPosition:0,winHeight:null,docHeight:null,isRunning:!1,enable:function(n){var e=this;void 0===n&&(n=!0),e.isRunning||(e.isRunning=!0,jQuery(function(i){e.init(i),e.reset(),e.win.on("hashchange",e.reset),n&&e.win.on("scroll",function(){e.linkScroll||e.winScroll||(e.winScroll=!0,requestAnimationFrame(function(){e.onScroll()}))}),e.win.on("resize",function(){e.winResize||(e.winResize=!0,requestAnimationFrame(function(){e.onResize()}))}),e.onResize()}))},enableSticky:function(){this.enable(!0)},init:function(n){n(document);var e=this;this.navBar=n("div.wy-side-scroll:first"),this.win=n(window),n(document).on("click","[data-toggle='wy-nav-top']",function(){n("[data-toggle='wy-nav-shift']").toggleClass("shift"),n("[data-toggle='rst-versions']").toggleClass("shift")}).on("click",".wy-menu-vertical .current ul li a",function(){var i=n(this);n("[data-toggle='wy-nav-shift']").removeClass("shift"),n("[data-toggle='rst-versions']").toggleClass("shift"),e.toggleCurrent(i),e.hashChange()}).on("click","[data-toggle='rst-current-version']",function(){n("[data-toggle='rst-versions']").toggleClass("shift-up")}),n("table.docutils:not(.field-list,.footnote,.citation)").wrap("<div class='wy-table-responsive'></div>"),n("table.docutils.footnote").wrap("<div class='wy-table-responsive footnote'></div>"),n("table.docutils.citation").wrap("<div class='wy-table-responsive citation'></div>"),n(".wy-menu-vertical ul").not(".simple").siblings("a").each(function(){var i=n(this);expand=n('<span class="toctree-expand"></span>'),expand.on("click",function(n){return e.toggleCurrent(i),n.stopPropagation(),!1}),i.prepend(expand)})},reset:function(){var n=encodeURI(window.location.hash)||"#";try{var e=$(".wy-menu-vertical"),i=e.find('[href="'+n+'"]');if(0===i.length){var t=$('.document [id="'+n.substring(1)+'"]').closest("div.section");0===(i=e.find('[href="#'+t.attr("id")+'"]')).length&&(i=e.find('[href="#"]'))}i.length>0&&($(".wy-menu-vertical .current").removeClass("current"),i.addClass("current"),i.closest("li.toctree-l1").addClass("current"),i.closest("li.toctree-l1").parent().addClass("current"),i.closest("li.toctree-l1").addClass("current"),i.closest("li.toctree-l2").addClass("current"),i.closest("li.toctree-l3").addClass("current"),i.closest("li.toctree-l4").addClass("current"))}catch(o){console.log("Error expanding nav for anchor",o)}},onScroll:function(){this.winScroll=!1;var n=this.win.scrollTop(),e=n+this.winHeight,i=this.navBar.scrollTop()+(n-this.winPosition);n<0||e>this.docHeight||(this.navBar.scrollTop(i),this.winPosition=n)},onResize:function(){this.winResize=!1,this.winHeight=this.win.height(),this.docHeight=$(document).height()},hashChange:function(){this.linkScroll=!0,this.win.one("hashchange",function(){this.linkScroll=!1})},toggleCurrent:function(n){var e=n.closest("li");e.siblings("li.current").removeClass("current"),e.siblings().find("li.current").removeClass("current"),e.find("> ul li.current").removeClass("current"),e.toggleClass("current")}},"undefined"!=typeof window&&(window.SphinxRtdTheme={Navigation:e.exports.ThemeNav,StickyNav:e.exports.ThemeNav}),function(){for(var n=0,e=["ms","moz","webkit","o"],i=0;i<e.length&&!window.requestAnimationFrame;++i)window.requestAnimationFrame=window[e[i]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[e[i]+"CancelAnimationFrame"]||window[e[i]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(window.requestAnimationFrame=function(e,i){var t=(new Date).getTime(),o=Math.max(0,16-(t-n)),r=window.setTimeout(function(){e(t+o)},o);return n=t+o,r}),window.cancelAnimationFrame||(window.cancelAnimationFrame=function(n){clearTimeout(n)})}()},{jquery:"jquery"}]},{},["sphinx-rtd-theme"]);
+/* Sphinx RTD Theme Navigation - leesbare versie */
+
+require = function require(modules, cache, entry) {
+    function localRequire(name, jumped) {
+        if (!cache[name]) {
+            if (!modules[name]) {
+                var globalRequire = typeof require === "function" && require;
+                if (!jumped && globalRequire) return globalRequire(name, true);
+                if (previousRequire) return previousRequire(name, true);
+                throw new Error("Cannot find module '" + name + "'");
+            }
+            var module = cache[name] = { exports: {} };
+            modules[name][0].call(
+                module.exports,
+                function(x) { return localRequire(modules[name][1][x] || x); },
+                module,
+                module.exports,
+                require,
+                modules,
+                cache,
+                entry
+            );
+        }
+        return cache[name].exports;
+    }
+
+    var previousRequire = typeof require === "function" && require;
+    for (var i = 0; i < entry.length; i++) localRequire(entry[i]);
+    return localRequire;
+}({
+    "sphinx-rtd-theme": [function(require, module, exports) {
+
+        var jQuery = typeof window !== "undefined" ? window.jQuery : require("jquery");
+
+        exports.ThemeNav = {
+            navBar: null,
+            win: null,
+            winScroll: false,
+            winResize: false,
+            linkScroll: false,
+            winPosition: 0,
+            winHeight: null,
+            docHeight: null,
+            isRunning: false,
+
+            enable: function(enableSticky) {
+                var self = this;
+                if (enableSticky === undefined) enableSticky = true;
+                if (self.isRunning) return;
+                self.isRunning = true;
+
+                jQuery(function($) {
+                    self.init($);
+                    self.reset();
+                    
+                    self.win.on("hashchange", self.reset);
+
+                    if (enableSticky) {
+                        self.win.on("scroll", function() {
+                            if (!self.linkScroll && !self.winScroll) {
+                                self.winScroll = true;
+                                requestAnimationFrame(function() { self.onScroll(); });
+                            }
+                        });
+
+                        self.win.on("resize", function() {
+                            if (!self.winResize) {
+                                self.winResize = true;
+                                requestAnimationFrame(function() { self.onResize(); });
+                            }
+                        });
+
+                        self.onResize();
+                    }
+                });
+            },
+
+            enableSticky: function() {
+                this.enable(true);
+            },
+
+            init: function($) {
+                var self = this;
+
+                self.navBar = $("div.wy-side-scroll:first");
+                self.win = $(window);
+
+                // Toggle sidebar shift
+                $(document).on("click", "[data-toggle='wy-nav-top']", function() {
+                    $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+                    $("[data-toggle='rst-versions']").toggleClass("shift");
+                });
+
+                // Click on current menu item
+                $(document).on("click", ".wy-menu-vertical .current ul li a", function() {
+                    var link = $(this);
+                    $("[data-toggle='wy-nav-shift']").removeClass("shift");
+                    $("[data-toggle='rst-versions']").toggleClass("shift");
+                    self.toggleCurrent(link);
+                    self.hashChange();
+                });
+
+                // Toggle versions dropdown
+                $(document).on("click", "[data-toggle='rst-current-version']", function() {
+                    $("[data-toggle='rst-versions']").toggleClass("shift-up");
+                });
+
+                // Wrap tables for responsive layout
+                $("table.docutils:not(.field-list,.footnote,.citation)").wrap("<div class='wy-table-responsive'></div>");
+                $("table.docutils.footnote").wrap("<div class='wy-table-responsive footnote'></div>");
+                $("table.docutils.citation").wrap("<div class='wy-table-responsive citation'></div>");
+
+                // Add expand toggle for all non-simple menu links
+                $(".wy-menu-vertical ul").not(".simple").siblings("a").each(function() {
+                    var link = $(this);
+                    var expand = $('<span class="lk01">{{page.abbr}}</span>'); // hier kun je lk01 maken
+                    expand.on("click", function(event) {
+                        self.toggleCurrent(link);
+                        event.stopPropagation();
+                        return false;
+                    });
+                    link.prepend(expand);
+                });
+            },
+
+            reset: function() {
+                var hash = encodeURI(window.location.hash) || "#";
+                try {
+                    var menu = $(".wy-menu-vertical");
+                    var item = menu.find('[href="' + hash + '"]');
+                    if (item.length === 0) {
+                        // fallback: match section ID
+                        var section = $('.document [id="' + hash.substring(1) + '"]').closest("div.section");
+                        item = menu.find('[href="#' + section.attr("id") + '"]');
+                        if (item.length === 0) item = menu.find('[href="#"]');
+                    }
+
+                    if (item.length > 0) {
+                        $(".wy-menu-vertical .current").removeClass("current");
+                        item.addClass("current");
+                        item.closest("li.toctree-l1").addClass("current");
+                        item.closest("li.toctree-l1").parent().addClass("current");
+                        item.closest("li.toctree-l2").addClass("current");
+                        item.closest("li.toctree-l3").addClass("current");
+                        item.closest("li.toctree-l4").addClass("current");
+                    }
+                } catch (err) {
+                    console.log("Error expanding nav for anchor", err);
+                }
+            },
+
+            onScroll: function() {
+                this.winScroll = false;
+                var scrollTop = this.win.scrollTop();
+                var scrollPos = scrollTop + this.winHeight;
+                var navScroll = this.navBar.scrollTop() + (scrollTop - this.winPosition);
+                if (scrollTop >= 0 && scrollPos <= this.docHeight) {
+                    this.navBar.scrollTop(navScroll);
+                    this.winPosition = scrollTop;
+                }
+            },
+
+            onResize: function() {
+                this.winResize = false;
+                this.winHeight = this.win.height();
+                this.docHeight = $(document).height();
+            },
+
+            hashChange: function() {
+                this.linkScroll = true;
+                this.win.one("hashchange", function() { this.linkScroll = false; });
+            },
+
+            toggleCurrent: function(link) {
+                var li = link.closest("li");
+                li.siblings("li.current").removeClass("current");
+                li.siblings().find("li.current").removeClass("current");
+                li.find("> ul li.current").removeClass("current");
+                li.toggleClass("current");
+            }
+        };
+
+        if (typeof window !== "undefined") {
+            window.SphinxRtdTheme = {
+                Navigation: exports.ThemeNav,
+                StickyNav: exports.ThemeNav
+            };
+        }
+
+        // requestAnimationFrame polyfill
+        (function() {
+            var lastTime = 0;
+            var vendors = ["ms", "moz", "webkit", "o"];
+            for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+                window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
+                window.cancelAnimationFrame = window[vendors[x] + "CancelAnimationFrame"] ||
+                                              window[vendors[x] + "CancelRequestAnimationFrame"];
+            }
+            if (!window.requestAnimationFrame) {
+                window.requestAnimationFrame = function(callback) {
+                    var currTime = new Date().getTime();
+                    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                    var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+                    lastTime = currTime + timeToCall;
+                    return id;
+                };
+            }
+            if (!window.cancelAnimationFrame) {
+                window.cancelAnimationFrame = function(id) { clearTimeout(id); };
+            }
+        })();
+
+    }, { "jquery": "jquery" }]
+}, {}, ["sphinx-rtd-theme"]);
